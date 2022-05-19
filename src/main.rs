3f8aa7d50f9175ff6ab25408a9fc5859e758769f
Sibaprasad Maiti <sibu.it13@gmail.com>
2022-05-19 11:01:37 +0530

    mock db connection and setup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
+use mongodb::{Client, Database};
+use rocket::{Rocket, State};
+use std::error::Error;
+
 #[macro_use]
 extern crate rocket;
 
+const DB_CONN_URL: &'static str =
+    "mongodb+srv://treatviewersdbusr:48MtSlonDyPbazuq@treatviewers.deppn.mongodb.net/treatviewers";
+const DB_NAME: &'static str = "treatviewers";
+
 #[get("/")]
-fn index() -> &'static str {
+async fn index(state: &State<DBConnPool>) -> &'static str {
+    let collections = state.mongo.list_collection_names(None).await.unwrap();
+    for coll_name in collections {
+        println!("collection: {:?}", coll_name);
+    }
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+struct DBConnPool {
+    mongo: Database,
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let client = Client::with_uri_str(DB_CONN_URL).await;
+    let client = client.unwrap();
+    let db = client.database(DB_NAME);
+    let pool = DBConnPool { mongo: db };
+    let _rocket = rocket::build()
+        .manage(pool)
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
 }
