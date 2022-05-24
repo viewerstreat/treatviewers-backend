ce30fb37cdb5500d2fc92a1437de5c4e98ede110
Sibaprasad Maiti <sibu.it13@gmail.com>
2022-05-24 10:57:46 +0530

    setup

---
+++ src/database.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use mongodb::{Client, Database};
+
+pub struct DBConnPool {
+    db: Database,
+}
+
+impl DBConnPool {
+    pub async fn new() -> Self {
+        let conn_url =
+            env::var("APP_DB_CONN_URL").expect("APP_DB_CONN_URL environment variable is not set");
+        let db_name = env::var("APP_DB_NAME").expect("APP_DB_NAME environment variable is not set");
+        let client = Client::with_uri_str(conn_url).await;
+        let client = client.expect("Not able to connect to database");
+        let db = client.database(&db_name);
+        Self { db }
+    }
+
+    pub fn get_db(&self) -> &Database {
+        &self.db
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,24 @@
-use mongodb::{Client, Database};
-use rocket::{Rocket, State};
-use std::error::Error;
+use database::DBConnPool;
+use rocket::State;
+
+mod database;
 
 #[macro_use]
 extern crate rocket;
 
-const DB_CONN_URL: &'static str =
-    "mongodb+srv://treatviewersdbusr:48MtSlonDyPbazuq@treatviewers.deppn.mongodb.net/treatviewers";
-const DB_NAME: &'static str = "treatviewers";
-
 #[get("/")]
 async fn index(state: &State<DBConnPool>) -> &'static str {
-    let collections = state.mongo.list_collection_names(None).await.unwrap();
+    let collections = state.get_db().list_collection_names(None).await.unwrap();
+    // let collections = state.db.list_collection_names(None).await.unwrap();
     for coll_name in collections {
         println!("collection: {:?}", coll_name);
     }
     "Hello, world!"
 }
 
-struct DBConnPool {
-    mongo: Database,
-}
-
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let client = Client::with_uri_str(DB_CONN_URL).await;
-    let client = client.unwrap();
-    let db = client.database(DB_NAME);
-    let pool = DBConnPool { mongo: db };
+    let pool = DBConnPool::new().await;
     let _rocket = rocket::build()
         .manage(pool)
         .mount("/", routes![index])
